1273bb7ea56707bf83c359b6a6677d84907a04a3
Pedro Cruz <pcruzmay@yahoo.com>
2020-08-17 16:20:01 -0500

    Git inicial

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    
+  </script>
+
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+  </style>
+
+<div class="Footer">
+    <div class="Footer-container">
+        <small class="Footer-content">
+            © 2020 Pugstagram from platzi
+        </small>
+    </div>
+</div>
